32e38f0c8b6188a02bbad92fd5f82649d0f15a3c
BHUSHAN RAMTEKE <bhushan7668@gmail.com>
2024-05-06 21:39:28 +0530

    initial MVVM template

---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace KeepMeOnline.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
---
+++ Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace KeepMeOnline.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
