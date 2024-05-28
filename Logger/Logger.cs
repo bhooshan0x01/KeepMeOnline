3b32b1a814674bf01865de00a0efb0bb3ad13139
BHUSHAN RAMTEKE <bhushan7668@gmail.com>
2024-05-28 23:29:08 +0530

    activity logs.

---
+++ Logger/Logger.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeepMeOnline
+{
+    internal class Logger
+    {
+        public static void LogMessage(string message)
+        {
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            string logFilePath = $"{path}\\KeepMeAliveLog.txt";
+            File.AppendAllText(logFilePath, $"{DateTime.Now}: {message}{Environment.NewLine}");
+        }
+    }
+}
