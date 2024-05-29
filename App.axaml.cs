f9466707a822e54994dc4745b7b3b872282a6ae3
BHUSHAN RAMTEKE <bhushan7668@gmail.com>
2024-05-29 17:23:24 +0530

    logs.

--- App.axaml.cs
+++ App.axaml.cs
@@ -19,104 +19,186 @@ public partial class App : Application
     private bool _isProgrammaticMovement;
     public override void Initialize()
     {
-        AvaloniaXamlLoader.Load(this);
+        
+        try
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+        catch (Exception ex)
+        {
+            Logger.LogMessage($"Error in Initialize: {ex.Message}");
+        }
 
     }
 
     public override void OnFrameworkInitializationCompleted()
     {
-        if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+        try
         {
-            desktop.Startup += OnStartup;
-            desktop.Exit += OnExit;
+            if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+            {
+                desktop.Startup += OnStartup;
+                desktop.Exit += OnExit;
 
+            }
+            base.OnFrameworkInitializationCompleted();
         }
-        base.OnFrameworkInitializationCompleted();
+        catch (Exception ex)
+        {
+            Logger.LogMessage($"Error in OnFrameworkInitializationCompleted: {ex.Message}");
+        }
     }
 
     private void OnStartup(object? sender, ControlledApplicationLifetimeStartupEventArgs e)
     {
-        _mouseHook = new GlobalMouseHook();
-        _mouseHook.MouseMoved += OnUserActivity;
+        
+        try
+        {
+            _mouseHook = new GlobalMouseHook();
+            _mouseHook.MouseMoved += OnUserActivity;
 
-        _keyboardHook = new GlobalKeyboardHook();
-        _keyboardHook.KeyPressed += OnUserActivity;
+            _keyboardHook = new GlobalKeyboardHook();
+            _keyboardHook.KeyPressed += OnUserActivity;
 
-        // 4 minutes timer
-        _inactiveTimer = new Timer(TimeSpan.FromSeconds(240));
-        _inactiveTimer.Elapsed += OnInactivityTimerElapsed;
+            // 4 minutes timer
+            _inactiveTimer = new Timer(TimeSpan.FromSeconds(240));
+            _inactiveTimer.Elapsed += OnInactivityTimerElapsed;
 
-        _sessionLockHandler = new SessionLockHandler();
-        _sessionLockHandler.SessionLocked += OnSessionLocked;
-        _sessionLockHandler.SessionUnlocked += OnSessionUnlocked;
+            _sessionLockHandler = new SessionLockHandler();
+            _sessionLockHandler.SessionLocked += OnSessionLocked;
+            _sessionLockHandler.SessionUnlocked += OnSessionUnlocked;
+        }
+        catch (Exception ex)
+        {
+            Logger.LogMessage($"Error in OnStartup: {ex.Message}");
+        }
     }
 
     private void OnSessionLocked(object? sender, EventArgs e)
     {
-        _isSessionLocked = true;
-        _inactiveTimer?.Stop();
+        try
+        {
+            _isSessionLocked = true;
+            _inactiveTimer?.Stop();
+        }
+        catch (Exception ex)
+        {
+            Logger.LogMessage($"Error in OnSessionLocked: {ex.Message}");
+        }
     }
 
     private void OnSessionUnlocked(object? sender, EventArgs e)
     {
-        _isSessionLocked = false;
-        ResetInactivityTimer();
+        try
+        {
+            _isSessionLocked = false;
+            ResetInactivityTimer();
+        }
+        catch (Exception ex)
+        {
+            Logger.LogMessage($"Error in OnSessionUnlocked: {ex.Message}");
+        }
     }
 
     private void OnUserActivity(object? sender, EventArgs e)
     {
-        Logger.LogMessage($"User Activity..");
+        
+        try
+        {
+            Logger.LogMessage($"User Activity..");
 
-        if (!_isProgrammaticMovement && !_isSessionLocked)
-        {
-            ResetInactivityTimer();
+            if (!_isProgrammaticMovement && !_isSessionLocked)
+            {
+                ResetInactivityTimer();
+            }
+            else
+            {
+                Logger.LogMessage($"Disable mouse simulation.");
+                _isProgrammaticMovement = false;
+            }
         }
-        else
+        catch (Exception ex)
         {
-            Logger.LogMessage($"Disable mouse simulation.");
-            _isProgrammaticMovement = false;
+            Logger.LogMessage($"Error in OnUserActivity: {ex.Message}");
         }
     }
 
     private void ResetInactivityTimer()
     {
-        Logger.LogMessage($"Resetting the timer... ");
-        _inactiveTimer?.Stop();
-        _inactiveTimer?.Start();
-        Logger.LogMessage($"Timer resetted.");
+        try
+        {
+            Logger.LogMessage($"Resetting the timer... ");
+            _inactiveTimer?.Stop();
+            _inactiveTimer?.Start();
+            Logger.LogMessage($"Timer resetted.");
+        }
+        catch (Exception ex)
+        {
+            Logger.LogMessage($"Error in ResetInactivityTimer: {ex.Message}");
+        }
     }
 
     private void OnInactivityTimerElapsed(object? sender, ElapsedEventArgs e)
     {
-        // Handle user inactivity 
-        if (!_isSessionLocked)
+        
+        try
         {
-            MoveMouse();
+            // Handle user inactivity 
+            if (!_isSessionLocked)
+            {
+                MoveMouse();
+            }
+        }
+        catch (Exception ex)
+        {
+            Logger.LogMessage($"Error in OnInactivityTimerElapsed: {ex.Message}");
         }
     }
 
     private void MoveMouse()
     {
-        _isProgrammaticMovement = true;
-        ImmortalMethods.SimulateMouseMovement();
+        try
+        {
+            _isProgrammaticMovement = true;
+            ImmortalMethods.SimulateMouseMovement();
+        }
+        catch (Exception ex)
+        {
+            Logger.LogMessage($"Error in MoveMouse: {ex.Message}");
+        }
     }
 
     private void CloseApplication(object? sender, EventArgs e)
     {        
-        Logger.LogMessage($"Exiting the app.");
-        _keyboardHook?.Dispose();
-        _mouseHook?.Dispose();
-        _inactiveTimer?.Dispose();
-        ImmortalMethods.AllowSleep();
-        _sessionLockHandler?.Dispose();
-        Environment.Exit(0); //OS will exit app immediately no OnExit event will be called
+        
+        try
+        {
+            Logger.LogMessage($"Exiting the app.");
+            _keyboardHook?.Dispose();
+            _mouseHook?.Dispose();
+            _inactiveTimer?.Dispose();
+            ImmortalMethods.AllowSleep();
+            _sessionLockHandler?.Dispose();
+            Environment.Exit(0); //OS will exit app immediately no OnExit event will be called
+        }
+        catch (Exception ex)
+        {
+            Logger.LogMessage($"Error in CloseApplication: {ex.Message}");
+        }
     }
 
     private void OnExit(object? sender, ControlledApplicationLifetimeExitEventArgs e)
     {
-        // TODO add additional logic for cleanup 
-        // this will not execute
-        Logger.LogMessage($"App exited.");
+        try
+        {
+            // TODO add additional logic for cleanup 
+            // this will not execute
+            Logger.LogMessage($"App exited.");
+        }
+        catch (Exception ex)
+        {
+            Logger.LogMessage($"Error in OnExit: {ex.Message}");
+        }
     }
 
 }
